9832cfed2ba10d68ab2957268cdf39f82c85d485
smitambalia <smit.ambalia@gmail.com>
2025-01-24 10:01:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Todo
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,7 @@
+export default function Todo({Todo}) {
+  return (
+    <div>
+      <h1>Todo</h1>
+    </div>
+  )
+}
