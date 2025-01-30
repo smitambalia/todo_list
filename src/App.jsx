275bc693a23617c8e865a7409dda8d9e5f446c93
smitambalia <smit.ambalia@gmail.com>
2025-01-30 19:22:07 +0530

    Removed unwanted imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import "./App.css";
 import AddTodo from "./components/AddTodo/AddTodo";
-import Todo from "./components/Todo/Todo";
 import TodoList from "./components/TodoList/TodoList";
 
 function App() {
