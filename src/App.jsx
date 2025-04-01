743c4c960673094adfd2a689c3733c847f7d0afc
smitambalia <smit.ambalia@gmail.com>
2025-04-01 10:58:58 +0530

    Supporting Children component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTodo from "./components/AddTodo/AddTodo";
 import TodoList from "./components/TodoList/TodoList";
 import ToDoContext from "./context/ToDoContext";
@@ -18,21 +18,71 @@ function App() {
     },
   ];
   const [todoList, settodoList] = useState(list);
+  const [counterCondition, setCounterCondition] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounterCondition((condition) => !condition);
+    }, 5000);
+  }, []);
 
   return (
-    <ToDoContext.Provider value={{todoList,settodoList}}>
+    <>
+    {/* <ToDoContext.Provider value={{ todoList, settodoList }}>
       <AddTodo
-        updateList={
-          (todo) =>
+        updateList={(todo) =>
           settodoList([
             ...list,
             { id: list + 1, todoData: todo, finished: false },
           ])
         }
       />
-      <TodoList/>
-    </ToDoContext.Provider>
+      <TodoList />
+      {counterCondition && <Counter> </Counter>}
+      
+    </ToDoContext.Provider>  */}
+    
+    <div style={{display : "flex"}}>
+    
+    <Card InnnerContent={
+       <div style={{color : "green"}}>
+         How do you want to post your todo?
+         <br/>
+         <input type="text" />
+       </div>
+      } />
+      <Card InnnerContent={"hi there"} />
+    </div>
+    </>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+    useEffect(function () {
+    setInterval(function ()  {
+      console.log("from inside the set interval")
+      setCount(count => count + 1);
+    }, 1000)
+  },[]);
+
+  return <h1>{count}</h1>;
+}
+
+function Card({ InnnerContent }) {
+  return (
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        padding: 10,
+        borderRadius: 10,
+        margin: 10,
+      }}
+    >
+      {InnnerContent}
+    </div>
+  );
+}
 export default App;
