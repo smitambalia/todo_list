4759a89de50d3435810f1b54c05e5d6fe0c39855
smitambalia <smit.ambalia@gmail.com>
2025-04-21 20:43:00 +0530

    Added Initial CSS for Countdown Component

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -2,10 +2,12 @@ export function Countdown() {
   return (
     <div className="flex bg-bisque-100 rounded-lg justify-center items-center font-sans-serif">
       <div>
-        <h1 className="text-3xl font-bold underline">Countdown timer app</h1>
-        <div id="input" className="rounded-lg bg-gray-200">
-          <input type="datetime-local" id="datetime" />
-          <button id="start">Start</button>
+        <h1 className="text-3xl font-bold">Countdown timer app</h1>
+        <div id="input" className="rounded-lg bg-red-200">
+          <input type="datetime-local" id="datetime" className="rounded-lg pl-2" />
+          <button id="start" type="submit" className="p-2">
+            Start
+          </button>
         </div>
         <div id="countdown-display" className="text-xl mt-4"></div>
       </div>
